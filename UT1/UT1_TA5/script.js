c4fc1926c31a9445bbe41d8b9d7595697976ed04
JPAmorin <109167051+JPAmorin@users.noreply.github.com>
2024-04-06 20:00:14 -0300

    cancel function

--- UT1/UT1_TA5/script.js
+++ UT1/UT1_TA5/script.js
@@ -1,47 +1,45 @@
-function createCard(button){
+function createCard(button) {
     let parent = button.parentElement;
     let newListItem = document.createElement("li")
     let text = document.createElement("input")
-    text.setAttribute("type","text")
-    text.setAttribute("placeholder","Enter a title for this card...")
+    text.setAttribute("type", "text")
+    text.setAttribute("placeholder", "Enter a title for this card...")
     let add = document.createElement("button")
-    add.textContent = "el que no anda"
+    add.textContent = "Add card"
     let cancel = document.createElement("button")
     cancel.textContent = "x"
     newListItem.appendChild(text)
     newListItem.appendChild(add)
     newListItem.appendChild(cancel)
     parent.appendChild(newListItem)
-    add.addEventListener("click",e => addCard(parent,text,add,cancel))
+    button.remove()
+    add.addEventListener("click", e => addCard(parent, text, add, cancel))
+    cancel.addEventListener("click", e => removeForm(parent, text, add, cancel))
 }
-function addCard(parent,text,add,cancel){
-    parent = parent.parentElement
-    let newTask = document.createElement("li")
-    let newPar = document.createElement("p")
-    newPar.textContent = text.value
-    newTask.appendChild(newPar)
-    text.remove()
-    add.remove()
-    cancel.remove()
-    parent.appendChild(newTask)
-    let newButton = document.createElement("button")
-    newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>Add item</button>"
-    parent.appendChild(newButton)
-    /*
-    if (text != ""){   
+function addCard(parent, text, add, cancel) {
+    if (text.value != "") {
         parent = parent.parentElement
         let newTask = document.createElement("li")
         let newPar = document.createElement("p")
-        newPar.textContent = text.value
+        newPar.innerHTML = `<p class = "chore">${text.value}</p>`
         newTask.appendChild(newPar)
+        text.remove()
+        add.remove()
+        cancel.remove()
         parent.appendChild(newTask)
-        let newListButton = document.createElement("li")
         let newButton = document.createElement("button")
-        newButton.setAttribute("class","addClassButton")
-        newButton.setAttribute("onclick","createCard(this)")
-        newButton.textContent = "Add item"
-        newListButton.appendChild(newButton)
-        parent.appendChild(newListButton)
+        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>Add a card</button>"
+        parent.appendChild(newButton)
+    } else {
+        removeForm(parent, text, add, cancel)
     }
-    */
+}
+function removeForm(parent, text, add, cancel) {
+    parent = parent.parentElement
+    text.remove()
+    add.remove()
+    cancel.remove()
+    let newButton = document.createElement("button")
+    newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>Add a card</button>"
+    parent.appendChild(newButton)
 }
