b5cd0bbc6c21db120c336504b937c44c2a789d58
JPAmorin <109167051+JPAmorin@users.noreply.github.com>
2024-04-09 15:15:26 -0300

    add list button

--- UT1/UT1_TA5/script.js
+++ UT1/UT1_TA5/script.js
@@ -45,7 +45,9 @@ function addCard(parent, text, add, cancel) {
         cancel.remove()
         parent.appendChild(newTask)
         let newButton = document.createElement("button")
-        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>Add a card</button>"
+        newButton.setAttribute("class", "button addClassButton")
+        newButton.setAttribute("onclick", "createCard(this)")
+        newButton.textContent = "+ Add a card"
         parent.appendChild(newButton)
     } else {
         removeForm(parent, text, add, cancel)
@@ -101,18 +103,46 @@ function removeForm(parent, text, add, cancel) {
     parent.appendChild(newButton)
 }
 function addTitle(titleContainer,text,add,cancel){
+    
+    let container = document.createElement("li")
+    let divContainer = document.createElement("div")
+    titleContainer
     let title = document.createElement("h2")
+    
+    let newOL = document.createAttribute("ol")
+    let newLI = document.createElement("li")
+    let newButton = document.createElement("button")
+
+    container.setAttribute("class","item")
     title.textContent = text.value
-    titleContainer.appendChild(title)
+    newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>+ Add card</button>"
     text.remove()
     add.remove()
     cancel.remove()
-    let newButton = document.createElement("button")
-    newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>+ Add card</button>"
-    parent = titleContainer.parent
-    let newLI = document.createElement("li")
+    
+    parent = document.getElementById("lists")
+    
+    titleContainer.appendChild(title)
     newLI.appendChild(newButton)
-    titleContainer.appendChild(newLI)
+    //newOL.appendchild(newLI)
+    
+
+    divContainer.appendChild(titleContainer)
+    //divContainer.appendChild(newLI)
+    divContainer.appendChild(newLI)
+    container.appendChild(divContainer)
+    parent.appendChild(container)
+
+    let newListButtonContainer = document.createElement("div")
+    newListButtonContainer.setAttribute("class", "list title")
+
+    let newListButton = document.createElement("button")
+    newListButton.setAttribute("class", "button")
+    newListButton.setAttribute("onclick", "createList(this)")
+    newListButton.textContent = "+ Add another list"
+    newListButtonContainer.appendChild(newListButton)
+
+    parent.appendChild(newListButtonContainer)
 }
 
 const openBtn = document.getElementById("openModal")
