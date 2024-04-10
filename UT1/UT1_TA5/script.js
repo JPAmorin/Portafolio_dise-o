df2ea528f06ebff341f9585b77d33427973f404b
Juan Pablo Amorín <JUAN.AMORINJ@correo.ucu.edu.uy>
2024-04-10 17:05:42 -0300

    orden

--- UT1/UT1_TA5/script.js
+++ UT1/UT1_TA5/script.js
@@ -16,23 +16,7 @@ function createCard(button) {
     add.addEventListener("click", e => addCard(parent, text, add, cancel))
     cancel.addEventListener("click", e => removeForm(parent, text, add, cancel))
 }
-function createList(button){
-    let parent = document.getElementById("list_container")
-    let titleContainer = button.parentElement
-    button.remove()
-    let text = document.createElement("input")
-    text.setAttribute("type", "text")
-    text.setAttribute("placeholder", "Enter list title...")
-    let add = document.createElement("button")
-    add.innerHTML = "Add card"
-    let cancel = document.createElement("button")
-    cancel.innerHTML = "x"
-    titleContainer.appendChild(text)
-    titleContainer.appendChild(add)
-    titleContainer.appendChild(cancel)
-    add.addEventListener("click", e => addTitle(titleContainer,text,add,cancel))
-    cancel.addEventListener("click", e => removeForm(parent,text,add,cancel))
-}
+
 function addCard(parent, text, add, cancel) {
     if (text.value != "") {
         parent = parent.parentElement
@@ -52,80 +36,49 @@ function addCard(parent, text, add, cancel) {
     } else {
         removeForm(parent, text, add, cancel)
     }
-}/*
-function addTitle(titleContainer,text,add,cancel){
-    if (text.value != ""){
-        let title = document.createElement("h2")
-        title.textContent = text.value
-        titleContainer.appendChild(title)
-        text.remove()
-        add.remove()
-        cancel.remove()
-        let parent = titleContainer.parentElement
-        let orderedList = document.createElement("ol")
-        orderedList.setAttribute("class","task list")
-        let firstListItem = document.createElement("li")
-        let newButton = document.createElement("button")
-        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>Add a card</button>"
-        firstListItem.appendChild(newButton)
-        orderedList.appendChild(firstListItem)
-        parent.appendChild(orderedList)
-        createAddListButton()
-    }
 }
-function createAddListButton(){
-    let newButton = document.createElement("button")
-    newButton.innerHTML = '<button class="button" class="addListButton" onclick="createList(this)">+ Add another list</button>'
-    let divInner = document.createElement("div")
-    divInner.setAttribute("class","list title")
-    let divOuter = document.createElement("div")
-    let newLI = document.createAttribute("li")
-    newLI.setAttribute("class", "item")
-    divInner.appendChild(newButton)
-    divOuter.appendChild(divInner)
-    newLI.appendChild(divOuter)
-    parent.appendChild(newLI)
-}*/
-function removeForm(parent, text, add, cancel) {
-    let newButton = document.createElement("button")
-    if (parent.itemIds == "list_container"){
-        parent = parent.parentElement
-        text.remove()
-        add.remove()
-        cancel.remove()
-        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createList(this)'>+ Add another list</button>"
-    } else {    
-        text.remove()
-        add.remove()
-        cancel.remove()
-        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>+ Add card</button>"
-    }
-    parent.appendChild(newButton)
+
+function createList(button) {
+    let parent = document.getElementById("list_container")
+    let titleContainer = button.parentElement
+    button.remove()
+    let text = document.createElement("input")
+    text.setAttribute("type", "text")
+    text.setAttribute("placeholder", "Enter list title...")
+    let add = document.createElement("button")
+    add.innerHTML = "Add card"
+    let cancel = document.createElement("button")
+    cancel.innerHTML = "x"
+    titleContainer.appendChild(text)
+    titleContainer.appendChild(add)
+    titleContainer.appendChild(cancel)
+    add.addEventListener("click", e => addTitle(titleContainer, text, add, cancel))
+    cancel.addEventListener("click", e => removeForm(parent, text, add, cancel))
 }
-function addTitle(titleContainer,text,add,cancel){
-    
+function addTitle(titleContainer, text, add, cancel) {
+
     let container = document.createElement("li")
     let divContainer = document.createElement("div")
     titleContainer
     let title = document.createElement("h2")
-    
+
     let newOL = document.createAttribute("ol")
     let newLI = document.createElement("li")
     let newButton = document.createElement("button")
 
-    container.setAttribute("class","item")
+    container.setAttribute("class", "item")
     title.textContent = text.value
     newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>+ Add card</button>"
     text.remove()
     add.remove()
     cancel.remove()
-    
+
     parent = document.getElementById("lists")
-    
+
     titleContainer.appendChild(title)
     newLI.appendChild(newButton)
     //newOL.appendchild(newLI)
-    
+
 
     divContainer.appendChild(titleContainer)
     //divContainer.appendChild(newLI)
@@ -145,12 +98,29 @@ function addTitle(titleContainer,text,add,cancel){
     parent.appendChild(newListButtonContainer)
 }
 
+function removeForm(parent, text, add, cancel) {
+    let newButton = document.createElement("button")
+    if (parent.itemIds == "list_container") {
+        parent = parent.parentElement
+        text.remove()
+        add.remove()
+        cancel.remove()
+        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createList(this)'>+ Add another list</button>"
+    } else {
+        text.remove()
+        add.remove()
+        cancel.remove()
+        newButton.innerHTML = "<button class='button' class='addClassButton' onclick='createCard(this)'>+ Add card</button>"
+    }
+    parent.appendChild(newButton)
+}
+
 const openBtn = document.getElementById("openModal")
 const closeBtn = document.getElementById("closeModal")
 const modal = document.getElementById("modal")
 openBtn.addEventListener("click", () => {
     modal.classList.add("open")
 })
-closeBtn.addEventListener("click",() => {
+closeBtn.addEventListener("click", () => {
     modal.classList.remove("open")
 })
